f6ac82531c26d817f56631135806538059c1bdcc
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-03-06 12:42:15 +0000

    testing

---
+++ Employee.js
@@ -0,0 +1,28 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        let role = "Employee";
+        return role;
+    }
+}
+
+module.exports = Employee;
+
+// add validation to ensure user input is in the proper format
---
+++ Intern.js
@@ -0,0 +1,21 @@
+const Employee = require('./Employee');
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+
+    getSchool() {
+        return this.school;
+    }
+
+    getRole() {
+        let role = "Intern";
+        return role;
+    }
+}
+
+
+
+// add validation to ensure user input is in the proper format
